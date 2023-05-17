df9f1d8f14cc4a7b335f03ae29f22dd1cf21a39a
andrey <larginator@gmail.com>
2023-05-17 19:43:10 +0600

    🎨 Architecture refactoring

--- lounge_team_bot/main.py
+++ lounge_team_bot/main.py
@@ -1,7 +1,7 @@
 from aiogram.utils import executor
 from config import dispatcher
-import group_admin_logic.handlers.command_handlers as group_admin_handlers
-import general_chats_logic.handlers.command_handlers as general_handlers
+import group_admin_logics.handlers.command_handlers as group_admin_handlers
+import general_chats_logics.handlers.command_handlers as general_handlers
 
 if __name__ == '__main__':
     group_admin_handlers.register_command_handlers(dispatcher)
